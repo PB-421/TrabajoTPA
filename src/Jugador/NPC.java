e8ecf4a269d2070f8a253efdf3a929d48c5a3aef
pablo <pablo.borderas@gmail.com>
2023-11-05 12:55:27 +0100

    Codigo_beta

---
+++ src/Jugador/NPC.java
@@ -0,0 +1,18 @@
+package Jugador;
+
+public class NPC extends Player {
+    public int Level;
+    public Car NPCCarCreation(int level) {
+        Car cochePlayer = new Car("lol", "rojo",
+                130 + (level*15),
+                4 + (level * 5));
+        cochePlayer.setAcceleration(accelerate(Level));
+        return cochePlayer;
+    }
+    public double accelerate(int level) {
+        random random = new random();
+        acceleration= random.Randomizer(level);
+        return acceleration;
+    }
+
+}
